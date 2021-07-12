e6990c9b85425145f9695ecb0b7ed15b9f3f721c
lavaskiller <kangrobowh@naver.com>
2021-07-12 21:48:08 +0900

    sub_aug

--- classification.py
+++ classification.py
@@ -72,15 +72,6 @@ num_classes = 5
 
 model = Sequential([
   layers.experimental.preprocessing.Rescaling(1./255, input_shape=(img_height, img_width, 3)),
-  keras.Sequential(
-  [
-    layers.experimental.preprocessing.RandomFlip("horizontal", 
-                                                 input_shape=(img_height, 
-                                                              img_width,
-                                                              3)),
-    layers.experimental.preprocessing.RandomRotation(0.1),
-    layers.experimental.preprocessing.RandomZoom(0.1),
-  ]),
   layers.Conv2D(16, 3, padding='same', activation='relu'),
   layers.MaxPooling2D(),
   Dropout(0.1),
